43892b45e9a8bb4d54f711753772b06a0b4b9f8a
michael-mvalue <mabsalon@maxvaluesoftware.com>
2025-06-28 02:17:11 -0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+import path from "path";
+
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+  }
 };
 
 export default nextConfig;
